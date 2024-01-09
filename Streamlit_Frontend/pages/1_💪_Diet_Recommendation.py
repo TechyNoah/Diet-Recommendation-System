f9261bd9d03b05aea5c097a421ff4023beadf318
TechyNoah <155831737+TechyNoah@users.noreply.github.com>
2024-01-09 18:24:23 +0100

    Update 1_💪_Diet_Recommendation.py update title

--- Streamlit_Frontend/pages/1_💪_Diet_Recommendation.py
+++ Streamlit_Frontend/pages/1_💪_Diet_Recommendation.py
@@ -5,9 +5,7 @@ from random import uniform as rnd
 from ImageFinder.ImageFinder import get_images_links as find_image
 from streamlit_echarts import st_echarts
 
-st.set_page_config(page_title="Noah Automatic Diet Recommendation", page_icon="💪🙌🙌",layout="wide")
-
-
+st.set_page_config(page_title="Noah's Context-Aware Diet Recommendation", page_icon="💪🙌🙌",layout="wide")
 
 nutritions_values=['Calories','FatContent','SaturatedFatContent','CholesterolContent','SodiumContent','CarbohydrateContent','FiberContent','SugarContent','ProteinContent']
 # Streamlit states initialization
